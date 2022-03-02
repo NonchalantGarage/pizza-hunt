2efaecd431b9e2a43dddae8c8f25c2db619e6b0f
curtis <hong.curtis@gmail.com>
2022-03-01 17:40:25 -0800

    added comment model and associated with Pizza model and added a virtual and getter

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
@@ -13,14 +19,18 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
         // if no pizza, send 404 status
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id" });
           return;
-    }
+        }
         res.json(dbPizzaData);
-
       })
       .catch((err) => {
         console.log(err);
@@ -47,18 +57,18 @@ const pizzaController = {
       .catch((err) => res.status(400).json(err));
   },
 
-//   delete
-  deletePizza ({params}, res){
-      Pizza.findOneAndDelete({_id:params.id})
-      .then(dbPizzaData =>{
-          if (!dbPizzaData) {
-              res.status(404).json({message: "No pizza found with this id!"});
-              return;
-          }
-          res.json(dbPizzaData);
+  //   delete
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((dbPizzaData) => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: "No pizza found with this id!" });
+          return;
+        }
+        res.json(dbPizzaData);
       })
-      .catch(err=>res.status(400).json(err));
-  }
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = pizzaController;
--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,45 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-        type: String
+      type: String,
     },
     createdBy: {
-        type: String
+      type: String,
     },
-    createdAt:{
-        type: Date,
-        default: Date.now
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
-        type:String,
-        default: 'Large'
+      type: String,
+      default: "Large",
     },
-    toppings:[]
+    toppings: [],
+    // Association to comment model
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters:true
+    },
+    id: false,
+  }
+);
+
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
 });
 
+const Pizza = model("Pizza", PizzaSchema);
 
-const Pizza = model('Pizza', PizzaSchema);
-
-module.exports =Pizza;
-
+module.exports = Pizza;
